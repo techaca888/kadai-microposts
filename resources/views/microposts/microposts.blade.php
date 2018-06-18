4a405b90a0b08480404375a50cb9000999353e28
fumiya.niikura <gitgitgiton@gmail.com>
2018-06-18 08:25:15 +0000

    delete favorite_button.blade.php

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -26,7 +26,7 @@
                 @endif
             </div>
             <div style="float: left;">
-                @if (Auth::user()->id == $micropost->user_id)
+                @if ($login_user->id == $micropost->user_id)
                     {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                         {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs ']) !!}
                     {!! Form::close() !!}
